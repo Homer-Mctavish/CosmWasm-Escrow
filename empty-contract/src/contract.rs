c8fd6e006be787606cd02e16baf11c5e0efe76d5
Homer-Mctavish <49701255+Homer-Mctavish@users.noreply.github.com>
2024-08-21 22:06:16 -0400

    additionofstates

--- empty-contract/src/contract.rs
+++ empty-contract/src/contract.rs
@@ -1,4 +1,4 @@
-use crate::msg::{AdminsListResp, GreetResp, InstantiateMsg, QueryMsg};
+use crate::msg::{AdminsListResp, ExecuteMsg, GreetResp, InstantiateMsg, QueryMsg};
 use crate::state::ADMINS;
 use cosmwasm_std::{
     to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
@@ -45,7 +45,65 @@ mod query {
         Ok(resp)
     }
 }
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub enum ExecuteMsg {
+    AddMembers { admins: Vec<String> },
+    Leave {},
+}
 
+#[allow(dead_code)]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> StdResult<Response> {
+    use ExecuteMsg::*;
+
+    match msg {
+        AddMembers { admins } => exec::add_members(deps, info, admins),
+        Leave {} => exec::leave(deps, info),
+    }
+}
+
+mod exec {
+    use cosmwasm_std::StdError;
+
+    use super::*;
+
+    pub fn add_members(
+        deps: DepsMut,
+        info: MessageInfo,
+        admins: Vec<String>,
+    ) -> StdResult<Response> {
+        let mut curr_admins = ADMINS.load(deps.storage)?;
+        if !curr_admins.contains(&info.sender) {
+            return Err(StdError::generic_err("Unauthorised access"));
+        }
+
+        let admins: StdResult<Vec<_>> = admins
+            .into_iter()
+            .map(|addr| deps.api.addr_validate(&addr))
+            .collect();
+
+        curr_admins.append(&mut admins?);
+        ADMINS.save(deps.storage, &curr_admins)?;
+
+        Ok(Response::new())
+    }
+
+    pub fn leave(deps: DepsMut, info: MessageInfo) -> StdResult<Response> {
+        ADMINS.update(deps.storage, move |admins| -> StdResult<_> {
+            let admins = admins
+                .into_iter()
+                .filter(|admin| *admin != info.sender)
+                .collect();
+            Ok(admins)
+        })?;
+
+        Ok(Response::new())
+    }
+}
 
 #[allow(dead_code)]
 pub fn execute(
@@ -63,6 +121,36 @@ mod tests {
     use cw_multi_test::{App, ContractWrapper, Executor};
 
     use super::*;
+
+    fn instantiation(){
+        let mut app = App::default();
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+        let addr = app.instantiate_contract(code_id, Addr::unchecked("owner"), &InstantiateMsg{admins: vec![]}, &[], "Contract", None).unwrap();
+
+        let resp: AdminsListResp = app.wrap().query_wasm_smart(addr, &QueryMsg::AdminsList {}).unwrap();
+        assert_eq!(resp, AdminsListResp{admins: vec![]});
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg {
+                    admins: vec!["admin1".to_owned(), "admin2".to_owned()],
+                },
+                &[],
+                "Contract 2",
+                None,
+            )
+            .unwrap();
+
+    let resp: AdminsListResp = app
+    .wrap()
+    .query_wasm_smart(addr, &QueryMsg::AdminsList {})
+    .unwrap();
+
+    assert_eq!(resp, AdminsListResp{admins: vec![Addr::unchecked("admin1"), Addr::unchecked("admin2")]});
+    }
 
     #[test]
     fn greet_query() {
--- empty-contract/src/lib.rs
+++ empty-contract/src/lib.rs
@@ -8,15 +8,21 @@ mod msg;
 mod state;
 
 #[entry_point]
-pub fn instantiate(deps: DepsMut, env: Env, info: MessageInfo, msg: InstantiateMsg)
-  -> StdResult<Response>
-{
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> StdResult<Response> {
     contract::instantiate(deps, env, info, msg)
 }
 
 #[entry_point]
-pub fn query(deps: Deps, env: Env, msg: msg::QueryMsg)
-  -> StdResult<Binary>
-{
+pub fn execute(deps: DepsMut, env: Env, info: MessageInfo, msg: ExecuteMsg) -> StdResult<Response> {
+    contract::execute(deps, env, info, msg)
+}
+
+#[entry_point]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
     contract::query(deps, env, msg)
 }
